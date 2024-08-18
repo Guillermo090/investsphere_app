1a79496ae12378e643849b0606c37eb174cfcf85
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2024-08-17 20:01:01 -0400

    initial vite commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import underConstructionLogo from './assets/under_construction.svg'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={underConstructionLogo}  style={{width: '300px', height: 'auto'}}  alt="React logo" />
+      </div>
+      <h1> En construcción... </h1>
+    </>
+  )
+}
+
+export default App
