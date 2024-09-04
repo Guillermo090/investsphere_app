16b5e0b87974d50c22ecea81f542c3f16d05aa97
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2024-09-03 21:26:26 -0400

    fix comments

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,7 @@
-import NavBar from '../components/NavBar'
 
 const Home = () => {
     return (
         <div>
-            <NavBar /> {/* Incluye la barra de navegación */}
 
             <h1>Home - Página Privada</h1>
             <p>Solo puedes ver esto si estás logueado.</p>
